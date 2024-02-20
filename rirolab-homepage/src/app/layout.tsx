9c48982fec89ac9a9d3e11e4504da1f15c2d6c11
alsichcan <jace.jisang.park@gmail.com>
2024-02-20 14:34:35 +0900

    Metadata Config

--- rirolab-homepage/src/app/layout.tsx
+++ rirolab-homepage/src/app/layout.tsx
@@ -2,6 +2,28 @@ import React from 'react';
 import 'src/app/globals.css';
 import Header from 'src/components/main/Header';
 import Footer from 'src/components/main/Footer';
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | KAIST RIRO Lab',
+    default: 'KAIST RIRO Lab', 
+  },
+  description: "Robust Intelligence and Robotics Laboratory in the School of Computing, KAIST led by Professor Daehyung Park, Ph.D.",
+  metadataBase: new URL('https://rirolab.kaist.ac.kr'),
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    title: 'KAIST RIRO Lab',
+    description: 'Robust Intelligence and Robotics Laboratory in the School of Computing, KAIST led by Professor Daehyung Park, Ph.D."',
+    url: 'https://rirolab.kaist.ac.kr',
+    siteName: 'KAIST RIRO Lab Homepage',
+    images: '/images/main/og.png',
+    locale: 'en_US',
+    type: 'website',
+  },
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
   return (
