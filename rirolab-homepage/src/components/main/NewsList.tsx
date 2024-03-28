1b6bc34f7a38c0e01dc03bac95c6ed09edf2b4ec
Jisang Park <jace.jisang.park@gmail.com>
2024-03-28 13:01:58 +0900

    news update

--- rirolab-homepage/src/components/main/NewsList.tsx
+++ rirolab-homepage/src/components/main/NewsList.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 const NewsList: React.FC = () => {
   const newsItems: Array<JSX.Element | string> = [
-    "Jaehwi, Chanyoung, Jinwoo achieved Master's Graduation in February 2024",
-    "Minjae's paper has been accepted to IEEE ICRA, 2024",
-    "Jaehwi's paper has been accepted to IEEE RA-L, 2023",
-    "Dohyun's paper has been accepted to AAAI, 2023",
-    'Prof. Park received the ICRA 2023 Outstanding Planning Paper Award',
+    "Jaehwi, Chanyoung, Jinwoo achieved Master's Graduation Jaehwi, Chanyoung, Jinwoo, Ju-chul received their Master's Degrees in February 2024!!",
+    "Minjae's paper has been accepted to IEEE ICRA, 2024!!",
+    "Dohyun's paper has been accepted to AAAI, 2024!!",
+    "Jaehwi's paper has been accepted to IEEE RA-L, 2023!!",
+    "Jaehwi is selected for the Fulbright Graduate Student Program!!",
+    'Prof. Park received the ICRA 2023 Outstanding Planning Paper Award!!',
     <>
       Prof. Park organizes a workshop on &quot;
       <a
@@ -29,9 +30,9 @@ const NewsList: React.FC = () => {
       </a>
       &quot; at RoMan, 2023
     </>,
-    'Dohyun and Jinwoo received the Best Student Paper Award at RiTA 2022',
-    "Yeseung's paper has been accepted to IEEE RA-L (IROS), 2022",
-    'Prof. Park received the Outstanding Young Researcher Award (우수신진연구자상) at ICROS, 2022',
+    'Dohyun and Jinwoo received the Best Student Paper Award at RiTA 2022!!',
+    "Yeseung's paper has been accepted to IEEE RA-L (IROS), 2022!!",
+    'Prof. Park received the Outstanding Young Researcher Award (우수신진연구자상) at ICROS, 2022!!',
     // 'Ulzhalgas received the Outstanding Paper Award at KROC 2022',
     // 'Selected for ICRA 2022 Outstanding Navigation Award Finalist',
   ];
